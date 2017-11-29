0bef8b7769483c33412b64fadaa27ffe3ffcb530
HanGyuLee <gksrbf95@hanmail.net>
2017-11-29 18:36:02 +0900

    mypage

--- FinalSNSRE/src/main/java/com/spring/hgl/controller/HglController.java
+++ FinalSNSRE/src/main/java/com/spring/hgl/controller/HglController.java
@@ -3,6 +3,8 @@ package com.spring.hgl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.hgl.service.InterHglService;
 
@@ -13,5 +15,12 @@ public class HglController {
 	@Autowired
 	private InterHglService service;
 	
+	@RequestMapping(value="/mypage.re", method={RequestMethod.GET})
+	public String mypage(){
+		
+		
+		return "hgl/mypage.tiles";
+		
+	}// end of login
 	
 }// end of HglController
